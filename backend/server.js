e5f835a926ceab9a31e7785a29da778affea716d
Paras Singh <sparas348@gmail.com>
2025-05-01 15:33:34 +0530

    4th part completed - socket implemented

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -7,15 +8,12 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-
-const app = express();
-const PORT = process.env.PORT || 5000;
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-// app.get("/", (req, res) => {
-//     res.send("Hello Monkey!");
-// });
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
@@ -24,7 +22,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server Running on port ${PORT}`);
 });
