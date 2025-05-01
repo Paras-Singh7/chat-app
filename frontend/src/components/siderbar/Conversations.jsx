3d19caa3bb38c6dd440e8f430c374de22a3f23f6
Paras Singh <sparas348@gmail.com>
2025-05-01 14:36:19 +0530

    3rd part completed - api integration

--- frontend/src/components/siderbar/Conversations.jsx
+++ frontend/src/components/siderbar/Conversations.jsx
@@ -1,16 +1,26 @@
-import Conversation from "./Conversation"
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
+import Conversation from "./Conversation";
 
 const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+
   return (
     <div className="py-2 flex flex-col overflow-auto">
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-    </div>
-  )
-}
+      {loading ? (
+        <span className="loading loading-spinner mx-auto"></span>
+      ) : null}
 
-export default Conversations
+      {conversations.map((conversation, idx) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+          emoji={getRandomEmoji()}
+          lastIdx={idx === conversations.length - 1}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Conversations;
